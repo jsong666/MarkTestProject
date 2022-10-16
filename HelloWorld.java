6d203a8f9ec5f51cb92f30228fd9135cccca6a3d
soushunchi <markjsong99@gmail.com>
2022-10-15 23:09:59 -0700

    "This is mark"

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     {
          System.out.println("Hello Word");
          System.out.println("This is a test");
+        System.out.println("This is Mark's test");
 
     }
 }
